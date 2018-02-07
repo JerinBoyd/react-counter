215ac7d3f7f5153628d17775cf8cdd56ffe4cb25
JerinBoyd <maxers@mac.com>
2018-02-07 12:38:35 -0500

    added a user sets value change increment or decrement // got the input button and got the binding at the top and correct input button but had to use Justins after that

--- src/App.js
+++ src/App.js
@@ -5,22 +5,28 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      count: 0
+      count: 0,
+      value: 1
+      
     }
     
     
     this.increment = this.increment.bind(this); //binding means inside of "increment" "this" keyword is the component 
     this.decrement = this.decrement.bind(this);
     this.reset = this.reset.bind(this);
+    this.handleValueChange = this.handleValueChange.bind(this);
   }
   increment(){
-    this.setState({count: this.state.count + 1}); 
+    this.setState({count: this.state.count + this.state.value}); 
   }
   decrement(){
-    this.setState({count: this.state.count - 1});
+    this.setState({count: this.state.count - this.state.value});
   }
   reset(){
     this.setState({count: 0});
+  }
+  handleValueChange(e){
+    this.setState({value: +e.target.value});
   }
   render() {
     return (
@@ -29,6 +35,8 @@ class App extends Component {
         <button onClick={this.decrement}>-</button>
         <h1>{this.state.count}</h1>
         <button onClick={this.reset}>Reset </button>
+        <input type="number" onChange={this.handleValueChange} value={this.state.value}/>
+        
       </div>
      
     );
