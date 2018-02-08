6b1d16d9f2f0f933706074be8aad14fc0e65135e
JerinBoyd <maxers@mac.com>
2018-02-07 20:33:19 -0500

    added login.js and attached to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Email from './Email'; 
+import LoginForm from './LoginForm';
 
 
 class App extends Component {
---
+++ src/LoginForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; 
+
+
+class LoginForm extends Component {
+    render(){
+        return(
+            <div className='jumbotron'>
+            <h1>ASDASD</h1>
+          </div>
+        );
+    }
+
+}
+
+
+export default LoginForm; 
