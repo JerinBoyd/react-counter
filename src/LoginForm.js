9b920c4f112e7b2f3b6a334fff6a766255147ba5
JerinBoyd <maxers@mac.com>
2018-02-07 20:57:51 -0500

    added alert to login button

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -19,7 +19,7 @@ class LoginForm extends Component {
                 <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary" onClick={()=>{ alert('alert'); }}>Submit</button>
         </form>
 
 
